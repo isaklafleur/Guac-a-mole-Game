2d2f06fba4e11207825cfb162cbb21ad398d324e
Isak La Fleur <isak.engdahl@gmail.com>
2017-04-16 16:04:23 +0200

    changed the logic of the game

--- js/main.js
+++ js/main.js
@@ -1,40 +1,85 @@
 function Game() {
-  this.molesArray = [];
-  this.answer = undefined;
   this.baseUrl = "./sounds/";
-  this.audio = ["correct.mp3", "incorrect.mp3"];
-}
-
-Game.prototype.createMolesArray = function (moles) {
-  for (var i = 0; i < moles; i++) {
-    this.molesArray.push(Math.floor(Math.random() * 5));
-  }
-  //console.log(this.molesArray)
-  return this.molesArray;
+  this.audio = ["eat_chips.m4a", "incorrect.mp3"];
+  this.gameScore = 0;
+  this.level = 1;
+  this.lives = 5;
+  this.playing = false;
 };
 
-Game.prototype.addMoleToHole = function () {
+Game.prototype.sequentialFadeIn = function (selectorText, speed, display, callback) {
+    display = typeof display !== 'undefined' ? display : "block";
 
+    var els = $(selectorText),
+        i = 0;
+
+    (function helper() {
+        els.eq(i++).fadeIn(speed, helper).css("display", display);
+        if (callback && i === els.length) {callback();}
+    })();
+}
+
+Game.prototype.addMoleToRandomHole = function () {
+  var that = this;
+  var moleTile = $('#' + Math.floor(Math.random() * 16 + 1));
+  moleTile.addClass('mole');
+
+  setTimeout(function(){
+    moleTile.removeClass('mole');
+    // that.gameScore-- does not work here.. I get he window object
+    // this.updateStats()  does not work...
+  }, Math.floor(Math.random() * 2000 + 1000));
 };
 
 Game.prototype.clickOnHole = function () {
   var that = this;
-  $('button').click(function() {
-    this.answer = $(this).attr('id');
-    that.playSound(this.answer);
+  this.gameOver();
+  $('.cell').click(function() {
+    if (!($(this).hasClass('mole'))) {
+      new Audio(that.baseUrl + that.audio[1]).play();
+      that.lives--
+      that.updateStats();
+    } else {
+      $(this).removeClass('mole');
+      that.gameScore++
+      that.updateStats();
+      new Audio(that.baseUrl + that.audio[0]).play();
+    }
   });
 };
 
-Game.prototype.playSound = function (answer) {
-  if (answer === 'correct') {
-    new Audio(this.baseUrl + this.audio[0]).play();
-  } else {
-    new Audio(this.baseUrl + this.audio[1]).play();
+Game.prototype.levelUp = function () {
+  this.level = Math.max(Math.floor(score / 10), 1);
+}
+
+Game.prototype.gameOver = function () {
+  if (lives === 0) {
+    clearInterval(getCells);
+    this.score = 0;
+    this.level = 1;
+    this.lives = 5;
+    this.playing = false;
   }
+}
+
+Game.prototype.startGame = function () {
+  var that = this;
+  setInterval(that.addMoleToRandomHole, Math.floor(Math.random()*3000 + 1000));
+};
+
+Game.prototype.updateStats = function () {
+  //this.levelUp()
+  $('#total').text(this.hits + this.misses);
+  $('#gameScore').text(this.gameScore);
+  $('#level').text(this.level);
+  $('#lives').text(this.lives);
 };
 
 $(document).ready(function () {
   var moleGame = new Game();
-  moleGame.createMolesArray(20);
-  moleGame.clickOnHole();
+  moleGame.sequentialFadeIn(".toBeFaddedIn", "fast", "inline-block", function() {
+    moleGame.startGame();
+    moleGame.clickOnHole();
+    console.log("I am just a callback");
+});
 });  
