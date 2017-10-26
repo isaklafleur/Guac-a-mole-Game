412ff7907fa698abad1085b1001197fb9adb9789
Isak La Fleur <isak.engdahl@gmail.com>
2017-10-26 20:01:39 +0200

    removed bootstrap and jquery deps.

--- js/main.js
+++ js/main.js
@@ -9,121 +9,116 @@ function Game() {
   this.level = 1;
   this.lives = 5;
   this.playing = false;
-};
-
-var scoreDisplay = document.getElementById("score-display");
-var cells = document.querySelectorAll(".cell");
-Game.prototype.displayScore = function () {
-  var that = this;
-    this.levelUp();
-    scoreDisplay.innerHTML = "Score: " + that.score + "<span id='level-display'> Level: " + that.level + "</span><span id='lifes-display'> Lives: " + that.lives + "</span>";
 }
 
-Game.prototype.levelUp = function () {
-  var that = this;
-  this.level = Math.max(Math.floor((this.score+10) / 10), 1);
-  switch(this.level) {
+const scoreDisplay = document.getElementById("score-display");
+const cells = document.querySelectorAll(".cell");
+Game.prototype.displayScore = function() {
+  const self = this;
+  this.levelUp();
+  scoreDisplay.innerHTML = `Score: ${self.score} <span id='level-display'>Level: ${self.level}</span><span id='lifes-display'> Lives: ${self.lives}</span>`;
+};
+
+Game.prototype.levelUp = function() {
+  const self = this;
+  this.level = Math.max(Math.floor((this.score + 10) / 10), 1);
+  switch (this.level) {
     case 2:
-    myAudio.playbackRate = 1.3;
-    myAudio.play();
-    this.speedRemoveClass = 900;
-    this.speedHighlight = 1600;
-    break;
+      myAudio.playbackRate = 1.3;
+      myAudio.play();
+      this.speedRemoveClass = 900;
+      this.speedHighlight = 1600;
+      break;
     case 3:
-    myAudio.playbackRate = 1.6;
-    myAudio.play();
-    this.speedRemoveClass = 800;
-    this.speedHighlight = 1200;
-    break; 
+      myAudio.playbackRate = 1.6;
+      myAudio.play();
+      this.speedRemoveClass = 800;
+      this.speedHighlight = 1200;
+      break;
     case 4:
-    myAudio.playbackRate = 1.9;
-    myAudio.play();
-    this.speedRemoveClass = 700;
-    this.speedHighlight = 800;
-    break;
+      myAudio.playbackRate = 1.9;
+      myAudio.play();
+      this.speedRemoveClass = 700;
+      this.speedHighlight = 800;
+      break;
     case 5:
-    myAudio.playbackRate = 2.1;
-    myAudio.play();
-    this.speedRemoveClass = 600;
-    this.speedHighlight = 800;
-    break;
+      myAudio.playbackRate = 2.1;
+      myAudio.play();
+      this.speedRemoveClass = 600;
+      this.speedHighlight = 800;
+      break;
   }
 };
 
-Game.prototype.randomCell = function () {
-    return Math.floor(Math.random() * 16);
+Game.prototype.randomCell = function() {
+  return Math.floor(Math.random() * 16);
 };
 
-Game.prototype.gameOver = function () {
-  var that = this;
+Game.prototype.gameOver = function() {
+  const self = this;
   if (this.lives === 0) {
-    clearInterval(that.getCells);
-    new Audio(that.baseUrl + that.audio[2]).play();
+    clearInterval(self.getCells);
+    new Audio(self.baseUrl + self.audio[2]).play();
     this.score = 0;
     this.level = 1;
     this.lives = 5;
     this.playing = false;
     myAudio.pause();
     myAudio.currentTime = 0;
-    $('#start').show();
+    document.getElementById("startbutton").style.display = "block";
   }
 };
 
 Game.prototype.highlightCell = function() {
-  var that = this;
-  var target = this.randomCell();
+  const target = this.randomCell();
   this.prevScore = this.score;
-  var moles = $('#' + Math.floor(Math.random() * 16 + 1));
-  moles.addClass('mole');
-  
-  setTimeout(function() {
-    moles.removeClass('mole');
-    if (that.score === that.prevScore) {
-      that.lives--;
-      that.displayScore();
-      that.gameOver();
+  const moles = document.getElementById(Math.floor(Math.random() * 16 + 1));
+  moles.classList.add("mole");
+
+  setTimeout(() => {
+    moles.classList.remove("mole");
+    if (this.score === this.prevScore) {
+      this.lives--;
+      this.displayScore();
+      this.gameOver();
     }
-  }, that.speedRemoveClass)
+  }, this.speedRemoveClass);
 };
 
-Game.prototype.startGame = function () {
-  var that = this;
-  $('#start').click(function() {
-    if (!(that.playing)) {
-      that.playing = true;
-      that.speedRemoveClass = 1000;
-      that.speedHighlight = 2000;
-      that.displayScore();
-      $(this).hide();
+Game.prototype.startGame = function() {
+  document.getElementById("start").addEventListener("click", () => {
+    if (!this.playing) {
+      this.playing = true;
+      this.speedRemoveClass = 1000;
+      this.speedHighlight = 2000;
+      this.displayScore();
+      document.getElementById("startbutton").style.display = "none";
       myAudio.playbackRate = 1;
       myAudio.play();
-      that.getCells = setInterval(function() {
-        that.highlightCell();
-      }, that.speedHighlight);
+      this.getCells = setInterval(() => {
+        this.highlightCell();
+      }, this.speedHighlight);
     }
   });
 };
 
-Game.prototype.checkClicks = function () {
-  var that = this;
-  $('.cell').click(function() {
-    if ($(this).hasClass('mole')) {
-      new Audio(that.baseUrl + that.audio[0]).play();
-      that.score++
-      that.displayScore()
-    } else {
-      new Audio(that.baseUrl + that.audio[1]).play();
-    }
-  })
+Game.prototype.checkClicks = function(e) {
+  if (e.currentTarget.classList.contains("mole")) {
+    new Audio(this.baseUrl + this.audio[0]).play();
+    this.score++;
+    this.displayScore();
+  } else {
+    new Audio(this.baseUrl + this.audio[1]).play();
+  }
 };
 
-var myAudio = document.createElement('audio');
-myAudio.setAttribute('src','./sounds/el_jarabe_tapatio.m4a');
+const myAudio = document.createElement("audio");
+myAudio.setAttribute("src", "./sounds/el_jarabe_tapatio.m4a");
 myAudio.loop = true;
 
-
-$(document).ready(function () {
-  var moleGame = new Game();
-    moleGame.startGame();
-    moleGame.checkClicks();
-});
+const moleGame = new Game();
+moleGame.startGame();
+const allCells = document.getElementsByClassName("cell");
+for (let i = 0; i < allCells.length; i++) {
+  allCells[i].addEventListener("click", event => moleGame.checkClicks(event));
+}
